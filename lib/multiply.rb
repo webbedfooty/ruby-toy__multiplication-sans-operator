7003c7cb26bd7d596f6e391cfc3e1726dd4cd443
webbedfooty <patrickwiechert@yahoo.com>
2016-03-05 00:43:44 +0000

    Updated

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,15 +1,20 @@
 def multiply(first_number, second_number)
-  multiply = String.new
-  first_number = 3
-  second_number = 10
+
   x = 0
   y = 0
   while x != first_number
     x = x + 1
     y = y + second_number
   end
-multiply = y
+return y
 end
+
+# updated comments:
+# It still works. I think I did a rake w/o the assigned values of 3 and 10 [see original code below] and it didn't work so I put those in.
+# I do like the use of Return y. I guess there was no need to have the multiply string in there; 
+# I probably would have skipped that if I knew about the Return.
+# cool
+
 
 # I didn't know it had to specifically come up with the answer 30
 #here is my original code to accept input
